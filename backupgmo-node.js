b34fc55bccd83c4f848a4f5dd85ea6ea78d86713
michael <semiquaver@mac.com>
2017-01-13 16:46:21 -0800

    fixed gmo-node
    Former-commit-id: 37f893cf8b680f7829abec400767862cdc4e8795

--- backupgmo-node.js
+++ backupgmo-node.js
@@ -1,23 +1,23 @@
 //trumpet is trombone
 
 let players = [
-    [0, 1, 3], //fl
-    [1, 3, 4], //ob
-    [0], //kl
-    [4], //bn
-    [2], //hn
+    [1, 2, 4], //fl
+    [2, 4, 5], //ob
+    [1], //kl
+    [5], //bn
+    [3], //hn
     // [], //tpt
-    [0, 1, 3, 5], //tn or tpt
+    [1, 2, 4, 6], //tn or tpt
     // tim
     // perc
     // fol
     // hl
     // hr
-    [0, 1, 2, 3, 4], //vn or vnII
-    [0, 1, 2, 3, 4], //vn
-    [0, 3], //va
-    [5], //vc
-    [4], //cb
+    [1, 2, 3, 4, 5], //vn or vnII
+    [1, 2, 3, 4, 5], //vn
+    [1, 4], //va
+    [6], //vc
+    [5], //cb
 ]
 
 let playernames = [
@@ -37,8 +37,6 @@ const input=require('get-stdin');
 input().then ( music => {
 let rawbars = music
 .split('%bn')
-.map(x=>x.trim())
-.slice(1)
 
 let bars = rawbars
 .map(m => m.split(/\n+/)
@@ -69,6 +67,7 @@ function assign(need, available = {
     3: true,
     4: true,
     5: true,
+    6: true
 }){
     if(need.length == 0) return [];
     else if(need.length == 1){
@@ -80,8 +79,8 @@ function assign(need, available = {
             let avprime = clone(available)
             delete avprime[player]
             let partial = assign(need.slice(1), avprime)
-            //  console.log(partial, avprime)
-            return  partial && [player, ...partial]
+            // console.log(partial, avprime)
+            return partial && [player, ...partial]
         }
 
         return get(need[0].filter(player => player in available).find(get))
@@ -93,7 +92,6 @@ function assign(need, available = {
 function solve(bar){
     let need = []
     let used = []
-
 
     bar.forEach((ne, i) =>{
         if(!ne) return;
@@ -107,34 +105,31 @@ function solve(bar){
     let sol = assign(need)
     let ret = [...bar]
 
-    sol && sol.forEach((p,i) => ret[used[i]] = p)
-    return sol && ret
+    sol.forEach((p,i) => ret[used[i]] = p)
+    return ret
 }
 
 
 // solve(nonemptybars[1])
 let sol = nonemptybars
 .map(solve)
-.forEach((bar, i) => {
-   // tex += '\nbarnumber: '+ b.barnumber
-   // if(bar){
-	let ret = ['rest','rest','rest','rest','rest','rest']
-	playernames.forEach((name, i) => {
-		ret[bar[i]] = name
-	})
-        console.log( 'parts:', ret.map((p, i) => i + 1+': ' + p).join(', '))
-   // } else {
-	let numparts=nonemptybars[i].reduce((a,b)=>a+b,0)
-	if(numparts>6) console.log('Error: too many parts', numparts )
-   // }
+.map((bar, i) => {
+    let ret = ['rest','rest','rest','rest','rest','rest']
+    playernames.forEach((name, i) => {
+        ret[bar[i]] = name
+    })
+    return {barnumber: 177+i, parts: ret, raw: rawbars[i]}
 })
 
+let tex = ''
 
-//sol
-//.forEach(b => {
-//    // tex += '\nbarnumber: '+ b.barnumber
-//    tex += '\nparts: '+ b.parts.map((p, i) => i + 1+': ' + p).join(', ')
-////    tex += '\nraw: \n%bn'+ b.raw
-//})
+sol
+.slice(1)
+.forEach(b => {
+    // tex += '\nbarnumber: '+ b.barnumber
+    tex += '\nparts: '+ b.parts.map((p, i) => i +': ' + p).join(', ')
+//    tex += '\nraw: \n%bn'+ b.raw
+})
 
-	}).catch(e=>console.log(e))
+console.log(tex)
+	});
